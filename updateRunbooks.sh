32c7f1401446cd4f148bb6539766a5ab917c77a8
Satish Talluri <satish@neptune.io>
2014-07-15 15:32:33 -0700

    Link Runbooks directory to static assets directory for one stop update and easy pull

--- updateRunbooks.sh
+++ updateRunbooks.sh
@@ -5,9 +5,13 @@ do
   script=`echo $line | cut -d":" -f1`
   runbookId=`echo $line | cut -d":" -f2`
   runbookName=$runbookId".runbook"
-  cp $script neptuneio-action-templates/$runbookName
+  cp $script runbooks/$runbookName
 done
 
+# Copy the neptuneio-action-templates directory to /resources directory where static assets reside
+
+# cp -r runbooks ~/workspace/neptuneio/src/main/webapp/resources/
+
 # Sync runbooks directory to S3
-s3cmd sync --exclude '.DS_Store' --add-header=Expires:max-age=604800 --acl-public neptuneio-action-templates s3://neptuneio-downloads/
+# s3cmd sync --exclude '.DS_Store' --add-header=Expires:max-age=604800 --acl-public neptuneio-action-templates s3://staging-neptuneio-downloads/resources/
 
