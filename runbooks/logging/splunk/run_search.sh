f9538d03a47714092049ec9c93cd10d9e6084f27
Kiran Gollu <kiran@neptune.io>
2015-11-04 06:29:02 -0800

    don't show progress bar but show errors for curl: splunk run_search runbook

--- runbooks/logging/splunk/run_search.sh
+++ runbooks/logging/splunk/run_search.sh
@@ -6,10 +6,10 @@ splunk_endpoint=https://ec2-54-165-131-64.compute-1.amazonaws.com:8089
 search_string="status>200"
 
 # run a search and capture search_id
-search_id=`curl -k -u $username:$password ${splunk_endpoint}/services/search/jobs -d search="search ${search_string}" | grep sid | cut -d ">" -f2 | cut -d "<" -f1`
+search_id=`curl -sS -k -u $username:$password ${splunk_endpoint}/services/search/jobs -d search="search ${search_string}" | grep sid | cut -d ">" -f2 | cut -d "<" -f1`
 
 # wait for a few seconds (increase as appropriate)
 sleep 5
 
 # fetch the results now
-curl -k -u $username:$password ${splunk_endpoint}/services/search/jobs/${search_id}/results --get -d output_mode=raw
+curl -sS -k -u $username:$password ${splunk_endpoint}/services/search/jobs/${search_id}/results --get -d output_mode=raw
