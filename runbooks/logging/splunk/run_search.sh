c704c68a843367df681b7a06a54226302c84f2e4
Kiran Gollu <kiran@neptune.io>
2015-11-04 06:23:25 -0800

    add time delay between search submission + querying search restuls for splunk run_search runbook

--- runbooks/logging/splunk/run_search.sh
+++ runbooks/logging/splunk/run_search.sh
@@ -8,5 +8,8 @@ search_string="status>200"
 # run a search and capture search_id
 search_id=`curl -k -u $username:$password ${splunk_endpoint}/services/search/jobs -d search="search ${search_string}" | grep sid | cut -d ">" -f2 | cut -d "<" -f1`
 
+# wait for a few seconds (increase as appropriate)
+sleep 5
+
 # fetch the results now
 curl -k -u $username:$password ${splunk_endpoint}/services/search/jobs/${search_id}/results --get -d output_mode=raw
