ca667c749cd56a28ffa2222a0257ed8c48fd51a0
Satish Talluri <satish@neptune.io>
2015-11-08 19:17:01 -0800

    Fix typos and messaging for splunk search runbook

--- runbooks/logging/splunk/run_search.sh
+++ runbooks/logging/splunk/run_search.sh
@@ -1,6 +1,6 @@
 # Splunk search runbook
 
-# Splunk credentails & endpoint here
+# Splunk credentials & endpoint here
 USERNAME=neptune-read-only-user
 PASSWORD=neptune
 SPLUNK_ENDPOINT=https://ec2-54-165-131-64.compute-1.amazonaws.com:8089
@@ -11,7 +11,7 @@ SEARCH_STRING="status>200"
 # Run a search and capture search_id 
 SEARCH_ID=`curl -sS -k -u $USERNAME:$PASSWORD ${SPLUNK_ENDPOINT}/services/search/jobs -d search="search ${SEARCH_STRING}" | grep sid | cut -d ">" -f2 | cut -d "<" -f1`
 
-# Wait for a few seconds (Increase waiting time as appropriate)
+# Wait for a few seconds for search to finish (Increase waiting time as appropriate)
 sleep 5
 
 # Fetch the search results now
