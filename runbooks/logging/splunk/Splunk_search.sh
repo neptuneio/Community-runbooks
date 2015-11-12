7d5f465107a6e5584ed8a240669af5911bdb4fbe
Anil Kumar Nalamalapu <aknalamalapu@neptune.io>
2015-11-12 14:02:34 -0800

    Remove hard coded splunk credentials

--- runbooks/logging/splunk/Splunk_search.sh
+++ runbooks/logging/splunk/Splunk_search.sh
@@ -1,18 +1,13 @@
 # Splunk search runbook
 
-# Splunk credentials & endpoint here
-USERNAME=neptune-read-only-user
-PASSWORD=neptune
-SPLUNK_ENDPOINT=https://ec2-54-165-131-64.compute-1.amazonaws.com:8089
-
 # Search string here
 SEARCH_STRING="status>200"
 
 # Run a search and capture search_id 
-SEARCH_ID=`curl -sS -k -u $USERNAME:$PASSWORD ${SPLUNK_ENDPOINT}/services/search/jobs -d search="search ${SEARCH_STRING}" | grep sid | cut -d ">" -f2 | cut -d "<" -f1`
+SEARCH_ID=`curl -sS -k -u $SPLUNK_USERNAME:$SPLUNK_PASSWORD ${SPLUNK_ENDPOINT}/services/search/jobs -d search="search ${SEARCH_STRING}" | grep sid | cut -d ">" -f2 | cut -d "<" -f1`
 
 # Wait for a few seconds for search to finish (Increase waiting time as appropriate)
 sleep 5
 
 # Fetch the search results now
-curl -sS -k -u $USERNAME:$PASSWORD ${SPLUNK_ENDPOINT}/services/search/jobs/${SEARCH_ID}/results --get -d output_mode=raw
+curl -sS -k -u $SPLUNK_USERNAME:$SPLUNK_PASSWORD ${SPLUNK_ENDPOINT}/services/search/jobs/${SEARCH_ID}/results --get -d output_mode=raw
