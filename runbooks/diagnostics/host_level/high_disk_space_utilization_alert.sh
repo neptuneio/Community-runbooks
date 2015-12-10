c3e7a2351a2239f2ef70c44b30d8c5b121b1d500
Buchi Reddy Busi Reddy <brbusireddy@neptune.io>
2015-12-09 18:59:25 -0800

    Runbook cleanup.

--- runbooks/diagnostics/host_level/high_disk_space_utilization_alert.sh
+++ runbooks/diagnostics/host_level/high_disk_space_utilization_alert.sh
@@ -3,22 +3,22 @@ df -h
 
 # Step 2: Find few largest files in logs directory which could be causing the fill up
 echo ""
-du -ah /var/log/ | sort -nr | head
+du -ah /var/log/ 2>/dev/null | sort -nr | head
 
 # To check the disk usage based on time
-# du -ah --time /var/log/
+# du -ah --time /var/log/ 2>/dev/null
 
 # Step3 : Check for /tmp and /cache files
 if [ -d "/tmp" ]; then
     echo ""
-    du -ah /tmp | sort -nr | head
+    du -ah /tmp 2>/dev/null | sort -nr | head
     echo ""
     df -h /tmp
 fi
 
 if [ -d "/cache" ]; then
     echo ""
-    du -ah /cache | sort -nr | head
+    du -ah /cache 2>/dev/null | sort -nr | head
     echo ""
     df -h /cache
 fi
