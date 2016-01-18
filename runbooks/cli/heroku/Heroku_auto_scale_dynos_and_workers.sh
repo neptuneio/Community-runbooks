ff0ac522837a87b2fcba7a6c7ba09fa4fac6bf39
Satish Talluri <satish@neptune.io>
2016-01-17 18:05:10 -0800

    Update Heroku auto-scaling runbook

--- runbooks/cli/heroku/Heroku_auto_scale_dynos_and_workers.sh
+++ runbooks/cli/heroku/Heroku_auto_scale_dynos_and_workers.sh
@@ -8,11 +8,15 @@ echo "--------------------"
 heroku ps --app $APP_NAME
 
 # Step 2:
-echo "Scaling up worker dynos"
+echo "Scaling up worker dynos upto a max limit"
 echo "---------------------------"
+workers_count=$(heroku ps --app $APP_NAME | grep "worker\." | wc -l)
+if [ "$workers_count" -lt 3 ]
+then
 heroku ps:scale worker+1 --app $APP_NAME
 # If required scale up to particular web dynos and worker count at once
 # heroku scale web=2 worker=3 --app $APP_NAME
+fi
 
 # Step 2:
 echo "OR Scaling down worker dynos maintaining a minimum of 1 worker dyno"
@@ -22,7 +26,7 @@ workers_count=$(heroku ps --app $APP_NAME | grep "worker\." | wc -l)
 if [ "$workers_count" -gt 1 ]
 then
   heroku ps:scale worker-1 --app $APP_NAME
-  # If required scale up to particular web dynos and worker count at once
+  # If required scale down to particular web dynos and worker count at once
   # heroku scale web=1 worker=1 --app $APP_NAME
 fi
 
