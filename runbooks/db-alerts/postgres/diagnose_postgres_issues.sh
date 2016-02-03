e9961c98d52971096c748af228500f8c98a6c8bd
Buchi Reddy Busi Reddy <brbusireddy@neptune.io>
2016-02-03 15:35:03 -0800

    Adding a postgres diagnosis runbook.

---
+++ runbooks/db-alerts/postgres/diagnose_postgres_issues.sh
@@ -0,0 +1,15 @@
+APP_NAME="test-app"
+
+# Check all the blocking connections and locks.
+heroku pg:blocking -a $APP_NAME
+heroku pg:locks -a $APP_NAME
+
+# Run a pg diagnose report.
+heroku pg:diagnose -a $APP_NAME
+
+# Check all the long running queries.
+heroku pg:long-running-queries -a $APP_NAME
+
+# Kill all pg connections, if you think that is ok.
+#heroku pg:killall -a $APP_NAME
+
