2ceca8de3196fc736d9685f6aafca23cedda28f8
Buchi Reddy Busi Reddy <brbusireddy@neptune.io>
2016-02-10 16:24:46 -0800

    Pulling out the min and max number of dynos and dyno type as variables so that users can quickly change them and use the runbook.

--- runbooks/cli/heroku/Heroku_auto_scale_dynos_and_workers.sh
+++ runbooks/cli/heroku/Heroku_auto_scale_dynos_and_workers.sh
@@ -1,6 +1,9 @@
 # Heroku Auto-scale Runbook
 
-APP_NAME="heroku_app_name_here"
+APP_NAME="your-app-name-here"
+DYNO_TYPE="worker"
+MAX_NUM_DYNOS=5
+MIN_NUM_DYNOS=1
 
 # Step 1:
 echo "Heroku Processes/dyno count before scaling"
@@ -8,26 +11,26 @@ echo "--------------------"
 heroku ps --app $APP_NAME
 
 # Step 2:
-echo "Scaling up worker dynos upto a max limit"
-echo "---------------------------"
-workers_count=$(heroku ps --app $APP_NAME | grep "worker\." | wc -l)
-if [ "$workers_count" -lt 3 ]
+dyno_count=$(heroku ps --app $APP_NAME | grep "$DYNO_TYPE\." | wc -l)
+if [ $dyno_count -lt $MAX_NUM_DYNOS ]
 then
-heroku ps:scale worker+1 --app $APP_NAME
-# If required scale up to particular web dynos and worker count at once
-# heroku scale web=2 worker=3 --app $APP_NAME
+  echo "Scaling up $DYNO_TYPE dynos upto a maximum of $MAX_NUM_DYNOS"
+  echo "---------------------------"
+  heroku ps:scale $DYNO_TYPE+1 --app $APP_NAME
+  
+  # If required scale up to particular web dynos and dyno count at once
+  # heroku scale web=2 $DYNO_TYPE=$MAX_NUM_DYNOS --app $APP_NAME
 fi
 
-# Step 2:
-echo "OR Scaling down worker dynos maintaining a minimum of 1 worker dyno"
+# OR Step 2:
+echo "Scaling down $DYNO_TYPE dynos to a minimum of $MIN_NUM_DYNOS"
 echo "---------------------------"
-workers_count=$(heroku ps --app $APP_NAME | grep "worker\." | wc -l)
-# Maintain a minimum of 1 worker dyno
-if [ "$workers_count" -gt 1 ]
+dyno_count=$(heroku ps --app $APP_NAME | grep "$DYNO_TYPE\." | wc -l)
+if [ $dyno_count -gt $MIN_NUM_DYNOS ]
 then
-  heroku ps:scale worker-1 --app $APP_NAME
-  # If required scale down to particular web dynos and worker count at once
-  # heroku scale web=1 worker=1 --app $APP_NAME
+  heroku ps:scale $DYNO_TYPE-1 --app $APP_NAME
+  # If required scale down to particular web dynos and dyno count at once
+  # heroku scale web=1 $DYNO_TYPE=$MIN_NUM_DYNOS --app $APP_NAME
 fi
 
 # Step 3:
