35abee0a766dc0102fefe30a58ca0b6d4c67855f
Satish Talluri <satish@neptune.io>
2016-02-29 14:57:58 -0800

    Clean up specific dyno restart runbook

--- runbooks/cli/heroku/Heroku_restart_dyno.sh
+++ runbooks/cli/heroku/Heroku_restart_dyno.sh
@@ -1,17 +1,18 @@
-# Heroku dyno restart runbook
+# Use the app and dyno information from trigger JSON
+# You might have to change the variables based on your trigger.
 
-# Get the app and dyno names from trigger data. You might have to 
-# change the variables based on your trigger.
 APP_NAME=$NP_TRIGGER_APP
 DYNO_NAME=$NP_TRIGGER_DYNO
 
 # Restart the dyno only if it's web or worker dyno.
-# You can comment this part if it's not required.
+
 if [[ $DYNO_NAME == *"web"* ]] || [[ $DYNO_NAME == *"worker"* ]];
 then
-  echo "Restarting heroku dyno"
-  echo "---------------------"
-  heroku ps:restart $DYNO_NAME --app $APP_NAME
+    echo "Restarting heroku dyno"
+    echo "---------------------"
+    heroku ps:restart $DYNO_NAME --app $APP_NAME
+else
+    echo "Not restarting anything as it's not a web or worker dyno"
 fi
 
 # Add more heroku CLI commands here...
